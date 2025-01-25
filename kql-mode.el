8ca8c27f55217e4ed62792f3b0640d0e4f7b6f71
Aimé Bertrand <aime.bertrand@macowners.club>
2025-01-25 13:39:32 +0100

    Update Instructions

--- kql-mode.el
+++ kql-mode.el
@@ -35,16 +35,25 @@
 ;; kql-mode provides syntax highlighting and basic indentation for Kusto Query Language (KQL).
 ;; To use, add the following to your Emacs configuration:
 ;;
-;; (add-to-list 'load-path "/path/to/kql-mode")
-;; (require 'kql-mode)
+;; I. Installation
+;;   A. Manual installation
+;;     1. Download the `kql-mode.el' file and add it to your `custom-load-path'.
+;;     2. In your `~/.emacs.d/init.el' or `~/.emacs':
+;;       (add-to-list 'auto-mode-alist '("\\.kql\\'" . kql-mode))
 ;;
-;; Example KQL code:
+;;   B. From Melpa
+;;     1. M-x package-install RET kql-mode RET.
+;;     2. In your `~/.emacs.d/init.el' or `~/.emacs':
+;;       (add-to-list 'auto-mode-alist '("\\.kql\\'" . kql-mode))
 ;;
-;; let startTime = ago(1d);
-;; Events
-;; | where Timestamp > startTime
-;; | summarize count() by bin(Timestamp, 1h)
-;; | render timechart
+;;   C. With `use-package'
+;;     (use-package kql-mode
+;;       :ensure t
+;;       :config
+;;       (add-to-list 'auto-mode-alist '("\\.kql\\'" . kql-mode)))
+;;
+;; II.  Activating the mode interactively
+;;    M-x kql-mode RET
 
 
 ;;; Code:
